4555c3acf20f075d8eb20d6616987959ac04483a
houcine1amraoui <houcine1amraoui@gmail.com>
2025-11-30 21:35:23 +0100

    up

--- src/components/user-reducer-data-fetching/FetchOnePost2.jsx
+++ src/components/user-reducer-data-fetching/FetchOnePost2.jsx
@@ -31,6 +31,9 @@ function FetchOnePost2() {
   useEffect(() => {
     const fetchOnePost = async () => {
       try {
+        // artificial delay
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
         const response = await fetch(
           `https://jsonplaceholder.typicode.com/posts/1`
         );
